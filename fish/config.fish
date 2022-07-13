4af94c10c4292a8badcdeee00ad2b4b827f40f5f
x2Clicking <84136037+x2Clicking@users.noreply.github.com>
2022-07-13 16:55:41 +0200

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    	set fish_greeting
+	alias s='thunar ~/Desktop/Screenshots/ &'
+	alias cat='bat --theme=base16-256'
+	alias ls='exa --group-directories-first'
+	alias tree='exa -T'
+	alias dev='cd ~/Desktop/Dev/'
+end
+
+function fish_right_prompt
+
+end
